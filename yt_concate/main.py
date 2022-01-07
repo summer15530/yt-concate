cfeec3497b8afee1eddf4d2e458a0bf72872fcbd
Mason Chou <summer15530@gmail.com>
2022-01-07 15:32:55 +0800

    Successfully got video urls from channel and requirements.txt

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,21 +1,25 @@
-
-
-
-import urllib
+import urllib.request
 import json
+from yt_concate.settings import API_KEY
+
+print(API_KEY)
+
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
+
 
 def get_all_video_in_channel(channel_id):
-    api_key = 'AIzaSyCMjjCFezWxaIbMx0EDklRexEGxcq7jvWQ'
+    api_key = api_key = 'AIzaSyCMjjCFezWxaIbMx0EDklRexEGxcq7jvWQ'
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key, channel_id)
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                        channel_id)
 
     video_links = []
     url = first_url
     while True:
-        inp = urllib.urlopen(url)
+        inp = urllib.request.urlopen(url)
         resp = json.load(inp)
 
         for i in resp['items']:
@@ -25,6 +29,10 @@ def get_all_video_in_channel(channel_id):
         try:
             next_page_token = resp['nextPageToken']
             url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
+        except KeyError:
             break
     return video_links
+
+
+video_list = get_all_video_in_channel(CHANNEL_ID)
+print(len(video_list))
